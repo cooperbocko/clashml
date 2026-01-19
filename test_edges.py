c71bc45de286c5b74073997fbf1326a54ff0d742
cooperbocko <cooperbocko@gmail.com>
2026-01-18 16:57:56 -0800

    testing edge

--- test_edges.py
+++ test_edges.py
@@ -1,7 +1,7 @@
 import time
 
 from edge import DetectEdge
-from control import Control, control
+from control import Control
 from config import Config
 
 config = Config.load_from_json('./configs/pc.json')
@@ -9,13 +9,15 @@ control = Control(
     config.screen_bounds.left,
     config.screen_bounds.top,
     config.screen_bounds.right,
-    config.screen_bounds.bottom
+    config.screen_bounds.bottom,
+    0.1
 )
-check_edge = DetectEdge(control, config.click_points.board, 5, 10)
+check_edge = DetectEdge(control, config.click_points.board, 20, 10)
 
 while True:
-    time.sleep(1)
+    time.sleep(3)
     screenshot = control.screenshot()
     board = check_edge.detect_edges(screenshot)
     print(board)
+    print()
 
